2831cbf8a2d1a67711293640eba4fb12eb79f2a4
Shadman <shadman.jamil@tenpearls.com>
2016-07-04 21:34:06 +0500

    added views; updated json resume form; added sample json

---
+++ js/app.js
@@ -0,0 +1,50 @@
+    var resumeApp = angular.module('resumeApp', ['ngRoute']);
+
+
+    resumeApp.config(function($routeProvider) {
+        $routeProvider.
+          when('/', {
+            templateUrl: 'views/resume-json.html',
+            controller: 'ResumeCtrl'
+          }).
+          when('/:resume', {
+            templateUrl: 'views/resume.html',
+            controller: 'ResumeParseCtrl'
+          }).
+          otherwise({
+            redirectTo: '/'
+          });
+    });
+
+
+    resumeApp.controller('ResumeCtrl', function ($scope, $http, $location, $rootScope){
+        new Clipboard('.btn');
+
+        $http.get('_sample.json').success(function(data) {
+          $scope.sample_json = data;
+        });
+
+        $scope.parseJson = function() {
+          if ($scope.enteredJson == undefined) {
+            $scope.message = "Please add json to generate resume";
+          } 
+          else {
+            try {
+                $scope.message = "Please wait..";
+                $rootScope.result = angular.fromJson($scope.enteredJson);
+                $location.path('resume');
+            } catch (e) {
+                $scope.message = "Please add valid json to generate resume";
+                $location.path('/');
+            }  
+          }
+        };
+    });
+
+
+    resumeApp.controller('ResumeParseCtrl', function ($scope, $routeParams, $rootScope){
+      $scope.name = $routeParams.resume;
+      $scope.data = $rootScope.result
+      console.log($rootScope.result.basic_information.first_name);
+    });
+
